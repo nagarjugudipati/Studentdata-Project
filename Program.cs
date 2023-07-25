31d12ab9bf0dff4736b4ff8b76f5ed1f33e6e991
nagarajugudipati1@gmail.com <nagarajugudipati1@gmail.com>
2023-07-25 14:46:32 +0530

    PracticeProject

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppPracticeProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\nagar\Desktop\Student Data.txt";
+
+            try
+            {
+                string fileContent = File.ReadAllText(filePath);
+                Console.WriteLine(fileContent);
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("File not found. Please make sure the file exists at the specified path.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred: " + ex.Message);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
